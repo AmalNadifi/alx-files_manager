479933608cd49986114cabf07e105b8e39720c27
AmalNadifi <amal.nadifi9@gmail.com>
2024-04-14 09:40:06 -0700

    index.js file updated

--- routes/index.js
+++ routes/index.js
@@ -6,28 +6,40 @@ import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
+// Route to get the status of the application
 router.get('/status', AppController.getStatus);
 
+// Route to get statistics about the application
 router.get('/stats', AppController.getStats);
 
+// Route to handle new user creation
 router.post('/users', UsersController.postNew);
 
+// Route to handle user authentication
 router.get('/connect', AuthController.getConnect);
 
+// Route to handle user logout
 router.get('/disconnect', AuthController.getDisconnect);
 
+// Route to get information about the currently logged-in user
 router.get('/users/me', UsersController.getMe);
 
+// Route to upload files
 router.post('/files', FilesController.postUpload);
 
+// Route to get a specific file by ID
 router.get('/files/:id', FilesController.getShow);
 
+// Route to get all files
 router.get('/files', FilesController.getIndex);
 
+// Route to publish a file
 router.put('/files/:id/publish', FilesController.putPublish);
 
+// Route to unpublish a file
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
+// Route to get file data
 router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
