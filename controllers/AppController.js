7bb80e5c45f597dbccfdb2ae48c9950e8c00a86a
AmalNadifi <amal.nadifi9@gmail.com>
2024-04-14 09:49:15 -0700

    AppControlleller.js file updated

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,16 +1,21 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
+// Controller for handling application-level requests
 class AppController {
   static getStatus(request, response) {
     response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
+  // Method to get statistics about users and files from the database
   static async getStats(request, response) {
-    const usersNum = await dbClient.nbUsers();
-    const filesNum = await dbClient.nbFiles();
-    response.status(200).json({ users: usersNum, files: filesNum });
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+
+    // Respond with the statistics as a JSON object
+    response.status(200).json({ users: usersCount, files: filesCount });
   }
 }
 
+// Export the AppController for use in other modules
 module.exports = AppController;
