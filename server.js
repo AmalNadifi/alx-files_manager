621d150231cd173e5c2abf80659c9b526d20af08
nadiaMahfoud <nadiamahfoud48@gmail.com>
2024-04-14 16:17:32 -0700

    updates

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const app = express();
 app.use(express.json());
 app.use('/', router);
 
-app.listen(port, () => {// Starting the server to listen on the specified port
+app.listen(port, () => {
+  // Starting the server to listen on the specified port
   console.log(`server running on port ${port}`);
 });
 
-export default app;// Exporting the Express application for testing purposes
+export default app; // Exporting the Express application for testing purposes
